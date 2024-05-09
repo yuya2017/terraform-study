a0515e5dbbca0aed43ce739042fb7b88152f00d0
yuya-2017 <yuya92220087@gmail.com>
2024-05-09 11:55:11 +0900

    fix: vpc test

--- hello.tf
+++ hello.tf
@@ -1,5 +1,18 @@
-resource "null_resource" "default" {
-  provisioner "local-exec" {
-    command = "echo 'Hello from Omeroid'"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
