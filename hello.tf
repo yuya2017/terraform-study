f4ab1c87b8b3ec27e8d30ac056e631f41323ecc4
yuya-2017 <yuya92220087@gmail.com>
2024-05-14 15:21:38 +0900

    fix: add internet gateway

--- hello.tf
+++ hello.tf
@@ -7,17 +7,43 @@ resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+# インターネットゲートウェイの作成
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# ルートテーブルの作成
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+}
+
 # Subnetの作成
 resource "aws_subnet" "my_subnet_a" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.0.0/24"
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "my_subnet_b" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1b"
+}
+
+# サブネットをルートテーブルに関連付ける
+resource "aws_route_table_association" "my_route_table_association_a" {
+  subnet_id      = aws_subnet.my_subnet_a.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+resource "aws_route_table_association" "my_route_table_association_b" {
+  subnet_id      = aws_subnet.my_subnet_b.id
+  route_table_id = aws_route_table.my_route_table.id
 }
 
 # IAM Roleの作成
