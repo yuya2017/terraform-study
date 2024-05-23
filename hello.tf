c531134d6cbb38be6d7ae77d2bd23bcdde50a73c
yuya-2017 <yuya92220087@gmail.com>
2024-05-23 11:41:46 +0900

    fix: add OIDCプロバイダ

--- hello.tf
+++ hello.tf
@@ -188,3 +188,22 @@ resource "aws_lb_listener" "my_listener" {
     target_group_arn = aws_lb_target_group.my_target_group.arn
   }
 }
+
+// OUIDプロパイダ設定
+data "http" "github_actions_openid_configuration" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
+data "tls_certificate" "github_actions" {
+  url = jsondecode(data.http.github_actions_openid_configuration.body).jwks_uri
+}
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github_actions.certificates[0].sha1_fingerprint]
+}
+
+output "alb_dns_name" {
+  value = aws_lb.my_lb.dns_name
+}
